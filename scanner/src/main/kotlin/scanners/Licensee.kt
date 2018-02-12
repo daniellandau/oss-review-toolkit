6cd241d755ee9fb51070abd5b8d38171042f2b20
Sebastian Schuberth <sebastian.schuberth@here.com>
2018-02-12 23:04:39 +0100

    Bootstrap Licensee
    This way we do not need to hard-code installation for scheduled CI
    builds.

--- scanner/src/main/kotlin/scanners/Licensee.kt
+++ scanner/src/main/kotlin/scanners/Licensee.kt
@@ -25,6 +25,7 @@ import com.here.ort.scanner.LocalScanner
 import com.here.ort.scanner.ScanException
 import com.here.ort.utils.OS
 import com.here.ort.utils.ProcessCapture
+import com.here.ort.utils.getCommandVersion
 import com.here.ort.utils.yamlMapper
 import com.here.ort.utils.log
 
@@ -34,15 +35,20 @@ object Licensee : LocalScanner() {
     override val scannerExe = if (OS.isWindows) "licensee.bat" else "licensee"
     override val resultFileExt = "yml"
 
-    // Licensee cannot report its version before https://github.com/benbalter/licensee/pull/269 which is not contained
-    // in any release yet.
-    override fun getVersion(executable: String) = "9.8.0"
+    override fun bootstrap(): File? {
+        ProcessCapture("gem", "install", "--user-install", "licensee", "-v", "9.9.0.beta.2").requireSuccess()
+        val ruby = ProcessCapture("ruby", "-rubygems", "-e", "puts Gem.user_dir").requireSuccess()
+        val userDir = ruby.stdout().trimEnd()
+        return File(userDir, "bin")
+    }
+
+    override fun getVersion(executable: String) = getCommandVersion(scannerPath.absolutePath, "version")
 
     override fun scanPath(path: File, resultsFile: File): Result {
         val process = ProcessCapture(
-                path.parentFile,
-                scannerExe,
-                path.name
+                scannerPath.absolutePath,
+                "detect",
+                path.absolutePath
         )
 
         if (process.stderr().isNotBlank()) {
